e5216f8d5743d1e8c2c8897ca6136db0d24f5e79
Mico Gerard O. Navarro <micogerardnavarro@gmail.com>
2026-09-21 20:14:55 +0800

    Add project setup guide to README and update branding assets

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'CivicGuard') }}</title>
+        <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
